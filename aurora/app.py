3c07f4a9cd8ba9f319cac592ee39dd94297548cc
Sabuat <sabuaturbina@yahoo.com>
2024-05-05 13:58:25 -0300

    Criação da estrutura dos templates do site. Aplicação do Parallax estrutura e estilo do site "contato.html"
    Changes to be committed:
    	modified:   aurora/app.py
    	new file:   aurora/static/contacto.css
    	new file:   aurora/static/fundo-aurora-1.jpg
    	new file:   aurora/static/fundo-aurora.jpg
    	modified:   aurora/static/styles.css
    	modified:   aurora/templates/barra.html
    	new file:   aurora/templates/blog.html
    	new file:   aurora/templates/contato.html
    	new file:   aurora/templates/loja.html
    	new file:   aurora/templates/sobre.html

--- aurora/app.py
+++ aurora/app.py
@@ -6,4 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/loja")
+def store():
+    return render_template("loja.html")
+
+@app.route("/sobre")
+def about():
+    return render_template("sobre.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contato")
+def contact():
+    return render_template("contato.html")
+
 app.run(debug=True)
