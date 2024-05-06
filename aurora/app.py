349a81805857ea2421eba763c3701ac5b0d9e624
Sabuat <sabuaturbina@yahoo.com>
2024-05-05 22:05:25 -0300

    Creating connectivity with Google Sheet
    Changes to be committed:
    	new file:   aurora/__pycache__/data.cpython-310.pyc
    	modified:   aurora/app.py
    	modified:   aurora/data.py
    	modified:   aurora/static/index.css
    	modified:   aurora/templates/barra.html
    	modified:   aurora/templates/blog.html
    	modified:   aurora/templates/index.html
    	new file:   site-aurora.json

--- aurora/app.py
+++ aurora/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, url_for
 
+from data import fonte
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", fontes = fonte)
 
 @app.route("/loja")
 def store():
@@ -16,6 +18,7 @@ def about():
 
 @app.route("/blog")
 def blog():
+    print(fonte)
     return render_template("blog.html")
 
 @app.route("/contato")
--- aurora/data.py
+++ aurora/data.py
@@ -1,2 +1,21 @@
-import pandas as pd
 import gspread
+from pathlib import Path
+
+#-- Google Spreadsheet -------------------------------------------------------------------------------
+
+# Configuração do caminho para o arquivo Json do google spreadsheets
+# Está sendo usada a biblioteca pathlib para permitir compatibilidade em diversos SO
+file_path = Path('../') / 'site-aurora.json'
+
+gc = gspread.service_account(file_path)
+db = gc.open('loja-online')
+
+Produtos = db.get_worksheet(0)
+Contatos = db.get_worksheet(1)
+
+fonte = {
+    "codigo" : Produtos.acell("A2").value,
+    "imagem" : Produtos.acell("B2").value,
+    "nome": Produtos.acell("C2").value,
+    "preco": Produtos.acell("D2").value,
+}
